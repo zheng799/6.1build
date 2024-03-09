d600da7049ec5b5c4a97f255a7d2ac85c4539370
zheng799 <125624602+zheng799@users.noreply.github.com>
2024-03-10 07:56:31 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -20,6 +20,9 @@ rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
 
 # 添加额外插件
+git clone --depth=1 https://github.com/imy7/luci-app-turboacc.git package/luci-app-turboacc
+git clone --depth=1 https://github.com/flytosky-f/luci-app-vlmcsd.git package/luci-app-vlmcsd
+git clone --depth=1 https://github.com/flytosky-f/openwrt-vlmcsd.git package/openwrt-vlmcsd
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
 git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
